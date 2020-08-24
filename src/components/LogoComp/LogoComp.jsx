b9bb7acb689d9eb3a91d7968842efbc87d547b82
jhnemogap <jhnemogap@gmail.com>
2020-08-24 18:57:42 -0500

    test 4 pre-commit using 'git commit -m'

--- src/components/LogoComp/LogoComp.jsx
+++ src/components/LogoComp/LogoComp.jsx
@@ -21,9 +21,8 @@ const LogoComp = (props) => {
   return (
     <picture style={{ ...stylesProps }}>
         <Link to={routeHome()}>
-          
           <img type="image/jpg" src={LogoImg} alt="Logo" width={width} />
-          
+
       </Link>
     </picture>
   )
