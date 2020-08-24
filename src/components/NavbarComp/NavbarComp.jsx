dc94e4e36c7fcd6b65ebefcb9b7f9590c3890f2b
jhnemogap <jhnemogap@gmail.com>
2020-08-24 18:48:30 -0500

    Finished UI HeaderLayout

--- src/components/NavbarComp/NavbarComp.jsx
+++ src/components/NavbarComp/NavbarComp.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 import { routeSearch } from "../../routes/paths";
 
 const NavbarComp = () => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Nav className="mr-auto">
-        <Nav.Item>
-          <Link to={routeSearch()} className="btn-outline-light">
-            Top Gifs
-          </Link>
-        </Nav.Item>
-      </Nav>
+      <Container>
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Link to={routeSearch()} className="btn-outline-light">
+              Top Gifs
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Container>
     </Navbar>
   );
 };
